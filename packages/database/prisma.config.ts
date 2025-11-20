9f4d332c39d312b36b4428ba003aeaad4fb4f36f
zomeru <zomerzxc.19@gmail.com>
2025-11-20 16:07:09 +0800

    config updates

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL") ?? process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL ?? env("DATABASE_URL"),
   },
 });
